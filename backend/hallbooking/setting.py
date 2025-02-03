c4d6ef4f492f52000f02c643425a66f326ca0490
Gidion0606 <ggidion897@gmail.com>
2025-02-03 05:05:21 -0800

    new updated

---
+++ backend/hallbooking/setting.py
@@ -0,0 +1,33 @@
+# settings.py
+
+import os
+ALLOWED_HOSTS = ['*']
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SECRET_KEY = 'django-insecure-$0$8$g$6$*#@$#^!@#&$%$^%$&%^&^%&^%&%^&%$^%$%^#@$#^!@#&$%$^%$&%^&^%&^%&%^&%$^%$%^#@'  # noqa
+
+
+DEBUG = False
+ALLOWED_HOSTS = ['54.152.58.221', 'halls.com', 'www.halls.com']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
