652b955b2c373da12fd11a0bc277f6a712aac644
Gidion0606 <ggidion897@gmail.com>
2025-02-05 06:47:13 -0800

    python2

--- backend/server/asgi.py
+++ backend/server/asgi.py
@@ -6,12 +6,19 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.server.settings')
+
+application = get_asgi_application()
+
 
 import os
 settings_modude = 'server.deploy_setting' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'server.settings'
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings_module')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings_module')
 
 application = get_asgi_application()
